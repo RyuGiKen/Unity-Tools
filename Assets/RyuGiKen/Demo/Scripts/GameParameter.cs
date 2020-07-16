1fc3a367ec4af63974401e390dc1cf85b9031d30
RyuGiKen <576074134@qq.com>
2020-07-16 10:01:18 +0800

    增加实现自动跟踪目标辅助功能的射箭场景。

---
+++ Assets/RyuGiKen/Demo/Scripts/GameParameter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 全局静态参数
+/// </summary>
+public static class GameParameter
+{
+    /// <summary>
+    /// 上下左右控制[-1，1]
+    /// </summary>
+    public static Vector2 ControlXY;
+    /// <summary>
+    /// 触发按键
+    /// </summary>
+    public static bool Fire;
+}
