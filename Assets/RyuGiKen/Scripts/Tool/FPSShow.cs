4f1ce5139a0ba9b302f57eb61c463f45514152cb
RyuGiKen <576074134@qq.com>
2020-08-14 10:29:33 +0800

    FPSShow启动帧率限制数值外放。增加F3帧数限制按键、F4帧数解锁按键。

--- Assets/RyuGiKen/Scripts/Tool/FPSShow.cs
+++ Assets/RyuGiKen/Scripts/Tool/FPSShow.cs
@@ -24,17 +24,32 @@ namespace RyuGiKen.Tools
         public bool autoAdjustQualityLevel = false;//根据帧数自动切换画质
         [Tooltip("最小帧数")] public int adjustMinFPS = 20;
         [Tooltip("最大帧数")] public int adjustMaxFPS = 50;
+        [Tooltip("限制帧率")] public int LockFrameRate = 60;
         private void Start()
         {
             if (FPSText == null)
             { FPSText = GetComponent<Text>(); }
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = LockFrameRate;
         }
         void Update()
         {
             if (Input.GetKeyUp(KeyCode.F1))//按F1降低画质
-            { QualitySettings.DecreaseLevel(); }
+            {
+                QualitySettings.DecreaseLevel();
+            }
             if (Input.GetKeyUp(KeyCode.F2))//按F2提高画质
-            { QualitySettings.IncreaseLevel(); }
+            {
+                QualitySettings.IncreaseLevel();
+            }
+            if (Input.GetKeyUp(KeyCode.F3))//按F3限制帧数为60;
+            {
+                Application.targetFrameRate = LockFrameRate;
+            }
+            if (Input.GetKeyUp(KeyCode.F4))//按F4不限制帧数;
+            {
+                Application.targetFrameRate = -1;
+            }
             if (FPSText != null)
             {
                 if (Input.GetKeyDown(hideKey))
