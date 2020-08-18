86a7dbd1b33608d852e95201afee5af83a13d5f9
RyuGiKen <576074134@qq.com>
2020-08-18 10:06:34 +0800

    FPSShow增加可选受时间缩放影响。

--- Assets/RyuGiKen/Scripts/Tool/FPSShow.cs
+++ Assets/RyuGiKen/Scripts/Tool/FPSShow.cs
@@ -14,16 +14,16 @@ namespace RyuGiKen.Tools
         [Tooltip("隐藏")] public bool hide = true;//默认隐藏
         public Text FPSText;
         float passTime;
-        float deltaTime = 0;
         float m_FPS;
         int FrameCount;
         [Tooltip("统计时间")] public float fixTime = 1;
         [Tooltip("切换按键")] public KeyCode hideKey = KeyCode.F7;
-
+        [Tooltip("不受时间缩放影响")] public bool UnscaledDeltaTime = true;
         [Header("根据帧数自动切换画质(连续10s)")]
-        public bool autoAdjustQualityLevel = false;//根据帧数自动切换画质
-        [Tooltip("最小帧数")] public int adjustMinFPS = 20;
-        [Tooltip("最大帧数")] public int adjustMaxFPS = 50;
+        [Tooltip("根据帧数自动切换画质")] public bool autoAdjustQualityLevel = false;
+        [Tooltip("帧率超范围计时")] static float deltaTime = 0;
+        [Tooltip("最小帧率")] public int adjustMinFPS = 20;
+        [Tooltip("最大帧率")] public int adjustMaxFPS = 50;
         [Tooltip("限制帧率")] public int LockFrameRate = 60;
         private void Start()
         {
@@ -36,11 +36,11 @@ namespace RyuGiKen.Tools
         {
             if (Input.GetKeyUp(KeyCode.F1))//按F1降低画质
             {
-                QualitySettings.DecreaseLevel();
+                AdjustQualityLevel(false);
             }
             if (Input.GetKeyUp(KeyCode.F2))//按F2提高画质
             {
-                QualitySettings.IncreaseLevel();
+                AdjustQualityLevel(true);
             }
             if (Input.GetKeyUp(KeyCode.F3))//按F3限制帧数为60;
             {
@@ -57,45 +57,58 @@ namespace RyuGiKen.Tools
                     hide = !hide;
                 }
                 FPSText.enabled = !hide;
-                FPS();
+                ShowFPS();
             }
-            if (autoAdjustQualityLevel && m_FPS < adjustMinFPS)//自动降低画质
+            if (autoAdjustQualityLevel && m_FPS < adjustMinFPS && Time.timeScale > 0)//自动降低画质
             {
                 if (deltaTime <= 0)
-                    deltaTime -= Time.deltaTime;
+                    deltaTime -= Time.unscaledDeltaTime;
                 else
                     deltaTime = 0;
                 if (deltaTime <= -10)
                 {
-                    QualitySettings.DecreaseLevel();
+                    AdjustQualityLevel(false);
+                    deltaTime = 0;
                 }
             }
-            else if (autoAdjustQualityLevel && m_FPS > adjustMaxFPS)//自动提高画质
+            else if (autoAdjustQualityLevel && m_FPS > adjustMaxFPS && Time.timeScale > 0)//自动提高画质
             {
                 if (deltaTime >= 0)
-                    deltaTime += Time.deltaTime;
+                    deltaTime += Time.unscaledDeltaTime;
                 else
                     deltaTime = 0;
                 if (deltaTime >= 10)
                 {
-                    QualitySettings.IncreaseLevel();
+                    AdjustQualityLevel(true);
                 }
             }
         }
         /// <summary>
-        /// 显示FPS
+        /// FPS计数
         /// </summary>
-        void FPS()
+        void ShowFPS()
         {
             FrameCount += 1;
-            passTime += Time.unscaledDeltaTime;//Time.deltaTime;
+            passTime += (UnscaledDeltaTime ? Time.unscaledDeltaTime : Time.deltaTime);
 
             if (passTime > fixTime)
             {
                 m_FPS = Mathf.RoundToInt(FrameCount / passTime);
-                FPSText.text = "FPS  " + m_FPS.ToString() + "  " + QualitySettings.GetQualityLevel().ToString();
+                FPSText.text = "FPS  " + m_FPS.ToString() + "  L" + QualitySettings.GetQualityLevel().ToString();
                 passTime = 0.0f;
                 FrameCount = 0;
+            }
+        }
+        public static void AdjustQualityLevel(bool isRise)
+        {
+            deltaTime = 0;
+            if (isRise)
+            {
+                QualitySettings.IncreaseLevel();
+            }
+            else
+            {
+                QualitySettings.DecreaseLevel();
             }
         }
     }
