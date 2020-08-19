7e76bbfea4fab9a8502b75bf23f72cc905f95070
RyuGiKen <576074134@qq.com>
2020-08-19 13:36:26 +0800

    增加清空日志功能。F5切换是否打印日志，F6清空日志。

--- Assets/RyuGiKen/Scripts/Common/Debug_T.cs
+++ Assets/RyuGiKen/Scripts/Common/Debug_T.cs
@@ -10,17 +10,43 @@ namespace RyuGiKen.Tools
     [DisallowMultipleComponent]
     public class Debug_T : MonoBehaviour
     {
+        public static bool Enable = true;
+        void LateUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.F5))//切换是否打印日志
+            {
+                Enable = !Enable;
+            }
+            if (Input.GetKeyDown(KeyCode.F6))//清空日志
+            {
+                Clear();
+            }
+        }
+        /// <summary>
+        /// 打印日志
+        /// </summary>
+        /// <param name="Content"></param>
         public static void Log(string Content)
         {
-            StreamWriter sw = new StreamWriter(Application.streamingAssetsPath + "/Log.txt", true);
-            string fileTitle = "[" + System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff") + "]";
-            sw.WriteLine(fileTitle);
-            //开始写入
-            sw.WriteLine(Content + "\r\n");
-            //清空缓冲区
-            sw.Flush();
-            //关闭流
-            sw.Close();
+            if (Enable)
+            {
+                StreamWriter sw = new StreamWriter(Application.streamingAssetsPath + "/Log.txt", true);
+                string fileTitle = "[" + System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff") + "]";
+                sw.WriteLine(fileTitle);
+                //开始写入
+                sw.WriteLine(Content + "\r\n");
+                //清空缓冲区
+                sw.Flush();
+                //关闭流
+                sw.Close();
+            }
+        }
+        /// <summary>
+        /// 清空日志
+        /// </summary>
+        public static void Clear()
+        {
+            File.WriteAllText(Application.streamingAssetsPath + "/Log.txt", string.Empty);
         }
     }
 }
