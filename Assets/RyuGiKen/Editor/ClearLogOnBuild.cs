95dfa07678cf770bb634d56b1df1b12de73c872a
RyuGiKen <576074134@qq.com>
2020-09-14 09:10:18 +0800

    增加构建时自动清空日志。修改Debug_T.Log直接打印为增加到字符串列表，通过另外的线程进行打印，减小因打印日志的消耗造成的影响。

---
+++ Assets/RyuGiKen/Editor/ClearLogOnBuild.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+/// <summary>
+/// 构建时清空日志
+/// </summary>
+public class ClearLogOnBuild : IPreprocessBuildWithReport
+{
+	public int callbackOrder { get; private set; }
+	public void OnPreprocessBuild(BuildReport report)
+	{
+		Debug.Log("构建时清空日志");
+		File.WriteAllText(Application.streamingAssetsPath + "/Log.txt", string.Empty);
+	}
+}
--- Assets/RyuGiKen/Scripts/Common/Debug_T.cs
+++ Assets/RyuGiKen/Scripts/Common/Debug_T.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using System.IO;
 using UnityEngine;
 namespace RyuGiKen.Tools
@@ -10,12 +11,35 @@ namespace RyuGiKen.Tools
     [DisallowMultipleComponent]
     public class Debug_T : MonoBehaviour
     {
+        public static Debug_T instance;
         [Tooltip("允许收录")] public static bool CanAdd = true;
         [Tooltip("允许打印")] public static bool CanPrint = false;
         public static string Path = Application.streamingAssetsPath;
+        [Tooltip("打印队列长度")] public int ListCount = 0;
+        [Tooltip("打印队列")] public static List<string> LogList = new List<string>();
+        Thread th;
+        static StreamWriter sw;
         private void Awake()
         {
+            if (!instance)
+                instance = this;
+            if (instance != this)
+                Destroy(this);
             Clear();
+            LogList.Clear();
+            th = new Thread(new ThreadStart(Print));
+            th.Start();
+        }
+        void Start()
+        {
+#if UNITY_EDITOR
+            CanPrint = false;//编辑器状态打印可能导致循环导入
+            CanAdd = false;
+#endif
+        }
+        void Update()
+        {
+            ListCount = LogList.Count;
         }
         private void LateUpdate()
         {
@@ -28,24 +52,52 @@ namespace RyuGiKen.Tools
                 Clear();
             }
         }
+        void Print()
+        {
+            while (true)
+            {
+                if (CanPrint && LogList.Count > 0)
+                {
+                    try
+                    {
+                        //StreamWriter sw = new StreamWriter(Path + "/Log.txt", true);
+                        int i = 0;
+                        //for (int i = 0; i < ((LogList.Count < 10) ? LogList.Count : 10); i++)
+                        {
+                            if (LogList[i] != "" && LogList[i] != null)
+                            {
+                                string fileTitle = "[" + System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff") + "]";
+                                sw.WriteLine(fileTitle);
+                                //开始写入
+                                sw.WriteLine(LogList[i] + "\r\n");
+                                //清空缓冲区
+                                sw.Flush();
+                                //关闭流
+                                //sw.Close();
+                            }
+                            LogList.RemoveAt(i);
+                        }
+                    }
+                    catch { }
+                }
+                Thread.Sleep(1);
+            }
+        }
         /// <summary>
         /// 打印日志
         /// </summary>
         /// <param name="Content"></param>
         public static void Log(string Content)
         {
-            if (CanPrint && Content != "" && Content != null)
-            {
-                StreamWriter sw = new StreamWriter(Path + "/Log.txt", true);
-                string fileTitle = "[" + System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff") + "]";
-                sw.WriteLine(fileTitle);
-                //开始写入
-                sw.WriteLine(Content + "\r\n");
-                //清空缓冲区
-                sw.Flush();
-                //关闭流
-                sw.Close();
-            }
+            if (CanAdd)
+                LogList.Add(Content);
+            Debug.Log(Content);
+        }
+        public static void LogError(string Content)
+        {
+            if (CanAdd)
+                LogList.Add(Content);
+            Debug.LogError(Content);
         }
         /// <summary>
         /// 清空日志
@@ -56,6 +108,19 @@ namespace RyuGiKen.Tools
             CanPrint = false;//清空前需暂停打印
             File.WriteAllText(Path + "/Log.txt", string.Empty);
             CanPrint = originEnable;
+            if (sw != null)
+                sw.Close();
+            sw = new StreamWriter(Path + "/Log.txt", true);
+        }
+        private void OnApplicationQuit()
+        {
+            if (th != null)
+                th.Abort();
+            if (sw != null)
+            {
+                sw.Flush();
+                sw.Close();
+            }
         }
     }
 }
