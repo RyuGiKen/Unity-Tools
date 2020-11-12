080260157f9acd6a8bb35f0080ad371e47831980
RyuGiKen <576074134@qq.com>
2020-11-12 10:27:23 +0800

    改进随机音乐播放机制。

--- Assets/RyuGiKen/Scripts/Tool/MusicRandomPlay.cs
+++ Assets/RyuGiKen/Scripts/Tool/MusicRandomPlay.cs
@@ -18,23 +18,37 @@ namespace RyuGiKen.Tools
         {
             audioSource = GetComponent<AudioSource>();
         }
+        private void OnEnable()
+        {
+            Switch();
+        }
         void Update()
         {
             if (audioSource != null && canPlay)
-                if (otherClip.Length > 0 && (!audioSource.isPlaying || Input.GetKeyUp(KeyCode.PageDown) || Input.GetKeyUp(KeyCode.PageUp)))//切换音乐
+                if (!audioSource.isPlaying || !this.gameObject.activeInHierarchy || Input.GetKeyUp(KeyCode.PageDown) || Input.GetKeyUp(KeyCode.PageUp))//切换音乐
                 {
-                    int randomNum = Random.Range(0, otherClip.Length);
-                    if (Input.GetKeyUp(KeyCode.PageUp))
-                        randomNum--;
-                    if (Input.GetKeyUp(KeyCode.PageDown))
-                        randomNum++;
-                    if (randomNum < 0)
-                        randomNum = otherClip.Length - 1;
-                    else if (randomNum >= otherClip.Length)
-                        randomNum = 0;
-                    audioSource.clip = otherClip[randomNum]; 
-                    audioSource.Play();
+                    Switch();
                 }
+        }
+        /// <summary>
+        /// 切换音乐
+        /// </summary>
+        public void Switch()
+        {
+            if (otherClip.Length > 0)
+            {
+                int randomNum = Random.Range(0, otherClip.Length);
+                if (Input.GetKeyUp(KeyCode.PageUp))
+                    randomNum--;
+                if (Input.GetKeyUp(KeyCode.PageDown))
+                    randomNum++;
+                if (randomNum < 0)
+                    randomNum = otherClip.Length - 1;
+                else if (randomNum >= otherClip.Length)
+                    randomNum = 0;
+                audioSource.clip = otherClip[randomNum];
+                audioSource.Play();
+            }
         }
     }
 }
