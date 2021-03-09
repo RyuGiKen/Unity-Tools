744d4ddf1ed4ccf6a45d9017ae6e67c65d2366f0
RyuGiKen <576074134@qq.com>
2021-03-09 16:51:26 +0800

    构建时生成Version文本，包含名称，发布版本号，Unity版本，打包日期，自动从工程目录删除。

---
+++ Assets/RyuGiKen/Editor/LogOnBuild.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+/// <summary>
+/// 构建时生成Version文本，自动删除
+/// </summary>
+public class LogOnBuild : IPreprocessBuildWithReport, IPostprocessBuildWithReport
+{
+	public int callbackOrder { get; private set; }
+	private DateTime m_startTime;
+	private string VersionFileFullName;
+	public void OnPreprocessBuild(BuildReport report)
+	{
+		m_startTime = DateTime.Now;
+		VersionFileFullName = Application.streamingAssetsPath + "/" + "Version.txt";
+		//File.Create(VersionFileFullName);
+		FileStream file = new FileStream(VersionFileFullName, FileMode.OpenOrCreate);
+		file.Close();
+
+		//File.WriteAllText(VersionFileFullName, RyuGiKen.Tools.Version.LogVersionDate());
+	}
+	public void OnPostprocessBuild(BuildReport report)
+	{
+		try
+		{
+			var endTime = DateTime.Now;
+			var deltaTime = endTime - m_startTime;
+			var hours = deltaTime.Hours.ToString("00");
+			var minutes = deltaTime.Minutes.ToString("00");
+			var seconds = deltaTime.Seconds.ToString("00");
+			Debug.LogFormat("构建耗时：{0}:{1}:{2}", hours, minutes, seconds);
+			File.Delete(VersionFileFullName);
+			File.Delete(VersionFileFullName + ".meta");
+		}
+		catch { }
+	}
+}
