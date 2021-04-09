c374c3a6db360e683d4452e10aea02eb8a63cde9
RyuGiKen <576074134@qq.com>
2021-04-09 14:39:22 +0800

    自动旋转组件增加可选全局或局部方向，增加可选启动随机方向，增加可选刷新时机。

--- Assets/RyuGiKen/Scripts/Tool/AutoRoll.cs
+++ Assets/RyuGiKen/Scripts/Tool/AutoRoll.cs
@@ -10,6 +10,15 @@ namespace RyuGiKen.Tools
     [AddComponentMenu("RyuGiKen/自动旋转")]
     public class AutoRoll : MonoBehaviour
     {
+        enum UpdateMode
+        {
+            Update,
+            LateUpdate,
+            FixedUpdate,
+        }
+        [SerializeField] Space space = Space.Self;
+        [SerializeField] UpdateMode m_UpdateMode;
+        [Tooltip("启动时随机方向")] [SerializeField] bool RandomStartAngle = false;
         [Tooltip("自动旋转")] public bool autoRoll;
         [Tooltip("旋转方向")] public Vector3 angle;
         [Tooltip("旋转速度")] public float rollSpeed;
@@ -20,15 +29,48 @@ namespace RyuGiKen.Tools
         [Tooltip("最大缩放")] public Vector3 maxScale;
         [Tooltip("缩放速度")] public float scaleSpeed;
         float scaleValue;
+        private void OnEnable()
+        {
+            if (RandomStartAngle)
+            {
+                this.transform.Rotate(angle.normalized * 360 * Random.value, space);
+            }
+        }
         void Start()
         {
             //originScale = this.transform.localScale;
         }
-
-        void Update()
+        private void Update()
+        {
+            if (m_UpdateMode == UpdateMode.Update)
+            {
+                Refresh(Time.deltaTime);
+            }
+        }
+        private void LateUpdate()
+        {
+            if (m_UpdateMode == UpdateMode.LateUpdate)
+            {
+                Refresh(Time.deltaTime);
+            }
+        }
+        private void FixedUpdate()
+        {
+            if (m_UpdateMode == UpdateMode.FixedUpdate)
+            {
+                Refresh(Time.fixedDeltaTime);
+            }
+        }
+        /// <summary>
+        /// 刷新
+        /// </summary>
+        /// <param name="step">步长</param>
+        void Refresh(float step)
         {
             if (autoRoll)
-                this.transform.Rotate(angle * rollSpeed * Time.deltaTime);
+            {
+                this.transform.Rotate(angle.normalized * rollSpeed * step, space);
+            }
             if (autoScale)
             {
                 //this.transform.localScale = Vector3.one * (Mathf.PingPong(Time.time * scaleSpeed, scale.y - scale.x) + scale.x);
