db99d65da6472ead8c585af2742bc5a288c24b59
RyuGiKen <576074134@qq.com>
2021-07-29 12:08:28 +0800

    自动旋转组件增加两角度间反复插值模式。

--- Assets/RyuGiKen/Scripts/Tool/AutoRoll.cs
+++ Assets/RyuGiKen/Scripts/Tool/AutoRoll.cs
@@ -18,10 +18,16 @@ namespace RyuGiKen.Tools
         }
         [SerializeField] Space space = Space.Self;
         [SerializeField] UpdateMode m_UpdateMode;
+        [Header("持续旋转")]
         [Tooltip("启动时随机方向")] [SerializeField] bool RandomStartAngle = false;
         [Tooltip("自动旋转")] public bool autoRoll;
         [Tooltip("旋转方向")] public Vector3 angle;
         [Tooltip("旋转速度")] public float rollSpeed;
+        [Header("反复旋转")]
+        [Tooltip("自动旋转")] public bool repeatRoll;
+        [Tooltip("旋转范围1")] public Vector3 minAngle;
+        [Tooltip("旋转范围2")] public Vector3 maxAngle;
+        [Header("反复缩放")]
         [Tooltip("自动缩放")] public bool autoScale;
         //Vector3 originScale;
         //public Vector2 scale;//[min-x,max-y]
@@ -29,11 +35,29 @@ namespace RyuGiKen.Tools
         [Tooltip("最大缩放")] public Vector3 maxScale;
         [Tooltip("缩放速度")] public float scaleSpeed;
         float scaleValue;
+        private void OnValidate()
+        {
+            CheckMode();
+        }
         private void OnEnable()
         {
-            if (RandomStartAngle)
+            CheckMode();
+            if (RandomStartAngle && (autoRoll || repeatRoll))
             {
-                this.transform.Rotate(angle.normalized * 360 * Random.value, space);
+                if (autoRoll)
+                    this.transform.Rotate(angle.normalized * 360 * Random.value, space);
+                else if (repeatRoll)
+                {
+                    switch (space)
+                    {
+                        case Space.Self:
+                            this.transform.localEulerAngles = ValueAdjust.Slerp(minAngle, maxAngle, Random.value);
+                            break;
+                        case Space.World:
+                            this.transform.rotation = ValueAdjust.Slerp(Quaternion.Euler(minAngle), Quaternion.Euler(maxAngle), Random.value);
+                            break;
+                    }
+                }
             }
         }
         void Start()
@@ -60,6 +84,12 @@ namespace RyuGiKen.Tools
             {
                 Refresh(Time.fixedDeltaTime);
             }
+            CheckMode();
+        }
+        void CheckMode()
+        {
+            if (autoRoll && repeatRoll)
+                repeatRoll = false;
         }
         /// <summary>
         /// 刷新
@@ -70,6 +100,18 @@ namespace RyuGiKen.Tools
             if (autoRoll)
             {
                 this.transform.Rotate(angle.normalized * rollSpeed * step, space);
+            }
+            else if (repeatRoll)
+            {
+                switch (space)
+                {
+                    case Space.Self:
+                        this.transform.localEulerAngles = ValueAdjust.Slerp(minAngle, maxAngle, Mathf.PingPong(Time.time * rollSpeed / 360f, 1));
+                        break;
+                    case Space.World:
+                        this.transform.rotation = ValueAdjust.Slerp(Quaternion.Euler(minAngle), Quaternion.Euler(maxAngle), Mathf.PingPong(Time.time * rollSpeed / 360f, 1));
+                        break;
+                }
             }
             if (autoScale)
             {
